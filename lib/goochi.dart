730048492d851b102da92252d3dc67f925e6111e
goochiDev <goodnessnzube7@gmail.com>
2024-03-12 08:58:57 +0100

    first commit

---
+++ lib/goochi.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+    appBar: AppBar(
+    title: const Text ('my first app',
+    style: TextStyle(
+    color: Colors.white
+    )),
+    centerTitle: true,
+
+    backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: ElevatedButton.icon(onPressed: () {},
+      icon: Icon(
+      Icons.mail
+      ), 
+      label: Text('mail me'),
+    ),
+    
+    )
+);
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:app/goochi.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:Home()
+));
+ 
